d406761b582f61f7f208363d648421ae24662e9e
federossi93 <rossifederico0993@gmail.com>
2022-11-28 12:16:26 +0100

    end

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+// Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario.
+// Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite.
+// Stampiamo a schermo tutte le partite con questo schema.
+//Olimpia Milano - Cantù | 55-60
+
+$casa = [ 'squadra'=>'Milano' ,'punteggio' => '55' ];
+
+$ospite = ['squadra'=>'Cantu' ,'punteggio' => '60' ];
+
+echo $casa['squadra'];
+echo ' - ';
+echo $ospite['squadra'];
+echo ' | ';
+echo $casa['punteggio'];
+echo ' - ';
+echo $ospite['punteggio'];
+
+
+?>
